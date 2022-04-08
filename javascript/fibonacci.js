29991d43070b4e71ce6ac82159a84d9bcb00ca49
Henry Miller <mail.henrym+git@gmail.com>
2022-04-08 11:34:34 -0400

    finished both

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,27 @@
+// 0, 1, 1, 2, 3, 5, 8
 function fibonacci(num) {
-  // type your code here
+  if (num < 2) {
+    return num
+  }
+
+  let tot = 0;
+  let num1 = 0;
+  let num2 = 1;
+  let newNum;
+  for(let i = 0; i < num-1; i++){
+    newNum = num1 +num2;
+    if (i >= num-2){
+      tot +=newNum
+      num1 = num2;
+      num2 = newNum;
+    } else {
+      num1 = num2;
+      num2 = newNum;
+    }
 }
+  return tot
+}
+
 
 if (require.main === module) {
   // add your own tests in here
--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,5 +1,21 @@
 def fibonacci(num)
-  # type your code in here
+  return num if num < 2
+
+  tot = 0;
+  num1 = 0;
+  num2 = 0;
+  num.times{|i| 
+    newNum = num1 +num2;
+    if i >= num-1
+      tot +=newNum
+    elsif i==0
+      num2 = 1;
+    else 
+      num1 = num2;
+      num2 = newNum;
+  end
+}
+ tot
 end
 
 if __FILE__ == $PROGRAM_NAME
